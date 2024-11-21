0353b7cb5b5b7dbbcbcbd79f4b1821b8f83e2d69
XroSilence <86516127+XroSilence@users.noreply.github.com>
2024-11-21 02:06:10 -0800

    Add styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'slate-900': '#0f172a',
+        'slate-800': '#1e293b',
+        'slate-700': '#334155',
+        'slate-300': '#cbd5e1',
+      },
+    },
   },
   plugins: [],
 }
